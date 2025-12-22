62778a56dd624ad8a6d3f6a7e96b5e8c59f15128
jasontang890789 <jasontang890789@gmail.com>
2025-12-22 14:45:49 +1100

    initial commit

---
+++ starter/starter/ml/model.py
@@ -0,0 +1,73 @@
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+def train_model(X_train, y_train):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.ndarray
+        Training data.
+    y_train : np.ndarray
+        Labels.
+    Returns
+    -------
+    model : RandomForestClassifier
+        Trained machine learning model.
+    """
+    rfc = RandomForestClassifier(random_state=42)
+
+    param_grid = {
+        'n_estimators': [200, 500],
+        'max_features': ['log2', 'sqrt'],
+        'max_depth': [4, 5, 100],
+        'criterion': ['gini', 'entropy']
+    }
+
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)   
+    
+    cv_rfc.fit(X_train, y_train)
+
+    return cv_rfc
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.ndarray
+        Known labels, binarized.
+    preds : np.ndarray
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, fbeta
+
+
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : RandomForestClassifier
+        Trained machine learning model.
+    X : np.ndarray
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.ndarray
+        Predictions from the model.
+    """
+    preds = model.best_estimator_.predict(X)
+
+    return preds
---
+++ starter/starter/train_model.py
@@ -0,0 +1,58 @@
+# Script to train machine learning model.
+import pandas as pd
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+
+import joblib
+
+# Add the necessary imports for the starter code.
+
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference 
+
+# Add code to load in the data.
+
+data_loc = "../data/census.csv"
+data = pd.read_csv(data_loc)
+data.columns = data.columns.str.strip()
+
+# Optional enhancement, use K-fold cross validation instead of a train-test split.
+train, test = train_test_split(data, test_size=0.20)
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+X_train, y_train, encoder, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+)
+
+# Proces the test data with the process_data function.
+
+X_test, y_test, encoder_test, lb_test = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+
+# Train and save a model.
+rfc = train_model(X_train, y_train)
+y_pred = inference(rfc, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+
+# Save model and encoder
+output_loc = "../model/"
+model_loc = output_loc + "model.pkl"
+encoder_loc = output_loc + "encoder.pkl"
+lb_loc = output_loc + "lb.pkl"
+
+joblib.dump(rfc, model_loc)
+joblib.dump(encoder, encoder_loc)
+joblib.dump(lb, lb_loc)
